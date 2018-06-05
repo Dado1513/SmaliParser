b9fa743680ca261dc41d57b7b23cfe3df7871db0
Dado1513 <dave.caputo93@gmail.com>
2018-06-05 13:19:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Smali Parser
+
+### Simple smali parser that analyze output dir produced by apktool to find how method was used and how parameters, based on list method passed on CLI
+
+```
+    Usage: python3 smaliparser.py -m loadUrl evaluateJavaScript -d apktooldiroutput
+```
---
+++ smaliparser.py
@@ -0,0 +1,165 @@
+import os
+import re
+import sys
+import threading
+import codecs
+import time
+import argparse
+
+file_2_method = dict()
+method_2_value = dict()
+all_url = list()
+
+def get_methods(content):
+    """
+    gets all methods in a single smali file content
+    :param content: smali file content
+    :rtype: list of lists
+    :return: [0] - method name
+                [1] - method parameters
+                [2] - method return value
+                [3] - method data
+    """
+    pattern_method_data = re.compile(r'^\.method.+?\ (.+?(?=\())\((.*?)\)(.*?$)(.*?(?=\.end\ method))', re.MULTILINE | re.DOTALL)
+
+    data = re.findall(pattern_method_data, content)
+    return data
+
+def get_called_methods(line):
+    """
+    gets all the method called inside a smali method data. works just fine with a single smali line
+    :param content: content of the smali data to be parsed
+    :rtype: list of lists
+    :return: [0] - called method parameters
+                [1] - called method object type
+                [2] - called method name
+                [3] - called method parameters object type
+                [4] - called method return object type
+    """
+    pattern_called_methods = re.compile(r'invoke-.*?\ {(.*?)}, (.+?(?=;))\;\-\>(.+?(?=\())\((.*?)\)(.*?)(?=$|;)', re.MULTILINE | re.DOTALL)
+    data = re.findall(pattern_called_methods, line)
+    return data
+
+def get_line(method_data):
+    """
+        from method of class get const value
+    """
+    patter_line = re.compile("[.line|.local*](.*?)(?=return|.line)", re.MULTILINE | re.DOTALL)
+    data = re.findall(patter_line,method_data)
+    return data
+
+def from_const_get_value(line,all_const):
+    """
+        from line and all const inside (v0,v1) ecc
+        get value of this if assigned at const
+    """
+    const = all_const.split(",")
+    name_to_value = dict()
+    for c in const:
+        c = c.replace(" ","")
+        pattern_const = re.compile("const.* {0}, (.*)".format(c))
+        data = re.findall(pattern_const,line)
+        
+        name_to_value[c] = data[-1] if len(data) > 0 else None
+    return name_to_value
+    #print(name_to_value)
+
+def find_url_inside(smali_file):
+    """
+        get list of all url inside smali file
+    """    
+    url_re = "https?:\/\/[a-zA-Z0-9@:%._\+~#=/][^\s|^\"|^)]+"
+    data = re.findall(url_re, smali_file)
+    # if len(data) > 0:
+        # all_url = list(set().union(all_url ,data))
+        
+
+
+def search_method(list_file,lock,list_method,all_url):
+    for file in list_file:
+        
+        if file.endswith(".smali"):
+            file_read = str(open(file,"r").read())
+            methods = get_methods(file_read)
+            for method in methods:
+                lines = get_line(method[3])
+                for line in lines:
+                    # print (line)
+                    method_called = get_called_methods(line) # tutti i metodi nella linea
+                    for m in method_called:
+                        if m[2] in list_method:
+                            # print(m[2])
+                            value = from_const_get_value(line,m[0]) # get value passed to method
+                            
+                            if file not in file_2_method.keys():
+                                file_2_method[file] = list()
+                                file_2_method[file].append(m[2]) # get method used in this file
+                            else:
+                                file_2_method[file] = list(set().union(file_2_method[file],[m[2]]))
+                            
+                            # lock.acquire()
+                            if m[2] not in method_2_value.keys() :
+                                method_2_value[m[2]] = list()
+                                method_2_value[m[2]].append(value)
+                            else:
+                                method_2_value[m[2]].append(value)
+                                
+                            # lock.release()
+                            # print()
+        try:
+            #file_read = str(open(file,"r").read())
+            if not file.endswith(".html"):
+                file_read = codecs.open(file,"r",encoding="utf-8",errors='ignore').read()
+                find_url_inside(file_read)
+        except Exception as e:
+            print("Exception file e {0}: {1} ".format(e,file))
+
+
+def start(dir, list_method):
+    
+    """
+        Smali Parser that be able to parsing smali code
+        and to get when method "method" has been used and the 
+        how parameter has been passed inside or if that method has been called
+    """
+    time_start = time.time()
+    threadLock = threading.Lock()
+    list_file = list()
+    dir_apk = dir
+    for root, dirs, files in os.walk(dir_apk):
+        for file in files:
+            list_file.append(os.path.join(root, file)) # append all file in list 
+    # list_method = ["loadUrl","addJavascriptInterface","evaluateJavaScript"]
+    threads = []
+    # print(len(list_file))
+    numero_thread_max = int(len(list_file) / 50) # ogni thread analizza 100 file
+    print("thread creati: {0}".format(numero_thread_max))
+    for i in range(0,numero_thread_max):
+        if i < numero_thread_max -1:
+            thread = threading.Thread(target=search_method,args=(list_file[i*numero_thread_max:(i+1)*numero_thread_max-1],threadLock,list_method,))
+        else:
+            thread = threading.Thread(target=search_method,args=(list_file[i*numero_thread_max:],threadLock,list_method,))
+        thread.start()
+        threads.append(thread)
+    for t in threads:
+        t.join()
+    print(file_2_method)
+    print(method_2_value)
+    time_end = time.time()
+    print("Exec in {0}".format(time_end - time_start))
+    # for file in files:
+        # if file.endswith(".smali") :
+
+def main():
+    parser = argparse.ArgumentParser(description="Smali Parser that search if method was used and with how parameter",
+                    usage="\n\ntpython smaliparser.py -l loadUrl evaluateJavaScript -d dircreatedapktool",
+                    epilog="Author: Davide Caputo")
+    parser.add_argument('-m','--methods', nargs='+', help='<Required> Set flag', required=True)
+    parser.add_argument('-d','--dir',help=" Dir to analyze",required=True)
+    args = parser.parse_args()
+
+    start(args.dir, args.methods)
+    
+
+if __name__ == "__main__":
+    main()
